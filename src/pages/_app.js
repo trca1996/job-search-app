3e11ceca5cdaaab3fd0def58e5519d245690419a
trca1996 <igor.trnko@yahoo.com>
2021-09-24 23:34:26 +0200

    added tailwindcss config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
